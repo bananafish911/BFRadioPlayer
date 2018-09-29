bb2c9885022aeb20463f7955db0eb5e69955b08c
Bananafish911 <victor.dombrovskiy@gmail.com>
2018-09-29 16:01:12 +0300

    initial

---
+++ Example/Pods/Target Support Files/BFRadioPlayer/BFRadioPlayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BFRadioPlayer : NSObject
+@end
+@implementation PodsDummy_BFRadioPlayer
+@end
---
+++ Example/Pods/Target Support Files/Pods-BFRadioPlayer_Tests/Pods-BFRadioPlayer_Tests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_BFRadioPlayer_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BFRadioPlayer_TestsVersionString[];
+
